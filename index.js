edf3fb4608f136381f41f6e4f57960de25d6becd
Fawn <sarahmargaret@ymail.com>
2023-09-06 15:35:41 -0400

    fixed script src for index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let count = 0;
 function increment() {
     count += 1;
     countEl.textContent = count;
+    console.log(count);
 }
 
 function save() {
@@ -12,5 +13,6 @@ function save() {
     saveEl.textContent += countStr;
     countEl.textContent = 0;
     count = 0;
+    console.log(count);
 }
 
